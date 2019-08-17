bc9152e1b30c05773569b8d31c888bb0b6f092a5
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-08-18 00:02:12 +0530

    Chrome name typo fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const get = () => {
   const homeDir = os.homedir();
   const username = os.userInfo().username;
   if (os.platform() === 'win32') {
-    return `C:\\Users\\${username}\\AppData\\Local\\Google\\Ghrome\\User Data\\Default\\Cookies`;
+    return `C:\\Users\\${username}\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Cookies`;
   }
 
   if (os.platform() === 'darwin') {
