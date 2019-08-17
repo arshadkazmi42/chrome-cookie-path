7c6a53d6201d2735000833e36545c150e131a004
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-08-17 23:57:51 +0530

    Remove unwanted try catch

--- tests/test.js
+++ tests/test.js
@@ -8,19 +8,13 @@ const ChromeCookiePath = require('../index');
 
 describe('test returned cookie path', () => {
   it('should return path of chrome coookie', () => {
-    try {
-      const path = ChromeCookiePath.get();
-      if (os.platform() === 'darwin') {
-        expect(path).to.equal(`${homeDir}/Library/Application Support/Google/Chrome/Default/Cookies`);
-      } else if(os.platform() === 'win32') {
-        expect(path).to.equal(`C:\\Users\\${username}\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Cookies`);
-      } else {
-        expect(path).to.equal(`${homeDir}/.config/google-chrome/Default/Cookies`);
-      }
-    } catch (err) {
-      if (os.platform() === 'win32') {
-        expect(err.message).to.equal('Windows is not supported');
-      }
+    const path = ChromeCookiePath.get();
+    if (os.platform() === 'darwin') {
+      expect(path).to.equal(`${homeDir}/Library/Application Support/Google/Chrome/Default/Cookies`);
+    } else if(os.platform() === 'win32') {
+      expect(path).to.equal(`C:\\Users\\${username}\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Cookies`);
+    } else {
+      expect(path).to.equal(`${homeDir}/.config/google-chrome/Default/Cookies`);
     }
   });
 });
